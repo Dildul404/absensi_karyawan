7950b611b552dd1b8dd8b3a7acfa70697d05b232
Alghifari Maula Abi <alghifarimaulaabi@gmail.com>
2026-05-19 19:45:01 +0700

    sql jam kerja

---
+++ jam_kerja.sql
@@ -0,0 +1,264 @@
+-- phpMyAdmin SQL Dump
+-- version 6.0.0-dev+20260429.663cbe73d8
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 19, 2026 at 12:44 PM
+-- Server version: 8.4.3
+-- PHP Version: 8.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jam_kerja`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `absen`
+--
+
+CREATE TABLE `absen` (
+  `id_absen` int NOT NULL,
+  `absen` enum('izin','sakit','alpha','hadir') NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `id_pekerja` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `absen`
+--
+
+INSERT INTO `absen` (`id_absen`, `absen`, `tanggal`, `id_pekerja`) VALUES
+(1, 'hadir', '2026-05-12 06:00:15', 1),
+(2, 'sakit', '2026-05-12 08:15:00', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cuti`
+--
+
+CREATE TABLE `cuti` (
+  `id_cuti` int NOT NULL,
+  `awal_cuti` date NOT NULL,
+  `akhir_cuti` date NOT NULL,
+  `id_pekerja` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `cuti`
+--
+
+INSERT INTO `cuti` (`id_cuti`, `awal_cuti`, `akhir_cuti`, `id_pekerja`) VALUES
+(1, '2026-05-29', '2026-07-01', 1),
+(2, '2026-05-15', '2026-05-28', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jam_kerja`
+--
+
+CREATE TABLE `jam_kerja` (
+  `id_jam_kerja` int NOT NULL,
+  `waktu_mulai` time NOT NULL,
+  `waktu_selesai` time NOT NULL,
+  `id_pekerja` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `jam_kerja`
+--
+
+INSERT INTO `jam_kerja` (`id_jam_kerja`, `waktu_mulai`, `waktu_selesai`, `id_pekerja`) VALUES
+(1, '06:00:30', '15:00:40', 1),
+(2, '18:00:00', '04:00:00', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lembur`
+--
+
+CREATE TABLE `lembur` (
+  `id_lembur` int NOT NULL,
+  `durasi_lembur` time NOT NULL,
+  `tanggal_lembur` date NOT NULL,
+  `id_pekerja` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `lembur`
+--
+
+INSERT INTO `lembur` (`id_lembur`, `durasi_lembur`, `tanggal_lembur`, `id_pekerja`) VALUES
+(1, '02:13:09', '2026-05-02', 1),
+(2, '03:20:15', '2026-06-02', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pekerja`
+--
+
+CREATE TABLE `pekerja` (
+  `id_pekerja` int NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `jabatan` enum('karyawan','manager') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `pekerja`
+--
+
+INSERT INTO `pekerja` (`id_pekerja`, `nama`, `jabatan`) VALUES
+(1, 'adi', 'karyawan'),
+(2, 'dada', 'manager');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `nama`, `email`, `password`) VALUES
+(1, 'saya', 'saya@gmail.com', '1234'),
+(2, 'eda', 'eda@example.com', 'eda4321');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `absen`
+--
+ALTER TABLE `absen`
+  ADD PRIMARY KEY (`id_absen`),
+  ADD KEY `id_pekerja` (`id_pekerja`);
+
+--
+-- Indexes for table `cuti`
+--
+ALTER TABLE `cuti`
+  ADD PRIMARY KEY (`id_cuti`),
+  ADD KEY `id_pekerja` (`id_pekerja`);
+
+--
+-- Indexes for table `jam_kerja`
+--
+ALTER TABLE `jam_kerja`
+  ADD PRIMARY KEY (`id_jam_kerja`),
+  ADD KEY `id_pekerja` (`id_pekerja`);
+
+--
+-- Indexes for table `lembur`
+--
+ALTER TABLE `lembur`
+  ADD PRIMARY KEY (`id_lembur`),
+  ADD KEY `id_pekerja` (`id_pekerja`);
+
+--
+-- Indexes for table `pekerja`
+--
+ALTER TABLE `pekerja`
+  ADD PRIMARY KEY (`id_pekerja`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `absen`
+--
+ALTER TABLE `absen`
+  MODIFY `id_absen` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `cuti`
+--
+ALTER TABLE `cuti`
+  MODIFY `id_cuti` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `jam_kerja`
+--
+ALTER TABLE `jam_kerja`
+  MODIFY `id_jam_kerja` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `lembur`
+--
+ALTER TABLE `lembur`
+  MODIFY `id_lembur` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `pekerja`
+--
+ALTER TABLE `pekerja`
+  MODIFY `id_pekerja` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `absen`
+--
+ALTER TABLE `absen`
+  ADD CONSTRAINT `absen_ibfk_1` FOREIGN KEY (`id_pekerja`) REFERENCES `pekerja` (`id_pekerja`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `cuti`
+--
+ALTER TABLE `cuti`
+  ADD CONSTRAINT `cuti_ibfk_1` FOREIGN KEY (`id_pekerja`) REFERENCES `pekerja` (`id_pekerja`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `jam_kerja`
+--
+ALTER TABLE `jam_kerja`
+  ADD CONSTRAINT `jam_kerja_ibfk_1` FOREIGN KEY (`id_pekerja`) REFERENCES `pekerja` (`id_pekerja`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `lembur`
+--
+ALTER TABLE `lembur`
+  ADD CONSTRAINT `lembur_ibfk_1` FOREIGN KEY (`id_pekerja`) REFERENCES `pekerja` (`id_pekerja`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
