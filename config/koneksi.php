58381ca97efae013db5e93ceed0cd601c55ce3d8
Dildul404 <justenjoycode09@gmail.com>
2026-05-20 06:54:01 +0700

    Adjust config

--- config/koneksi.php
+++ config/koneksi.php
@@ -6,14 +6,10 @@ $database = "jam_kerja";
 $charset = "utf8mb4";
 
 $dsn = "mysql:host=$local;dbname=$database;charset=$charset";
+$conn = new PDO($dsn, $user, $password);
 
 try {
-    $koneksi = new PDO($dsn, $user, $password);
-    $koneksi->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Koneksi berhasil";
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Koneksi gagal: " . $e->getMessage();
 }
-
-
-?>
