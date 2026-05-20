9d00b190e756585e78ffcc5d301826ca9267d440
Alghifari Maula Abi <alghifarimaulaabi@gmail.com>
2026-05-20 07:02:15 +0700

    update index

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Absensi Karyawan</title>
+    <link rel="stylesheet" href="/karyawan/public/css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <style type="text/tailwindcss">
+      @theme {
+        --color-clifford: #da373d;
+      }
+    </style>
+</head>
+<body class="bg-gray-400/40 min-h-screen flex items-center justify-center">
+
+
+</body>
+</html>
